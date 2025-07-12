ee10a896c604b6aaefaf38c5b4d11d1960a7bcb3
Dominik Jur <jur.dominik@gmail.com>
2025-07-12 16:34:15 +0200

    fixed imputation in preprocessing

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -15,38 +15,54 @@ def preprocess_data(
     standardize=True,
     categorical_features=[],
 ):
-
     if impute:
-        imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
-        X_train = imputer.fit_transform(X_train)
-        X_test = imputer.transform(X_test)
-
-    if onehot:
-
         def make_pd_from_np(x):
             data = pd.DataFrame(x)
             for c in categorical_features:
-                data.iloc[:, c] = data.iloc[:, c].astype("int")
+                data.iloc[:, c] = data.iloc[:, c].astype("category")
             return data
-
-        X_train, X_test = make_pd_from_np(X_train), make_pd_from_np(X_test)
+        
+        X_train_df = make_pd_from_np(X_train)
+        X_test_df = make_pd_from_np(X_test)
+        
+        numerical_features = [i for i in range(X_train_df.shape[1]) if i not in categorical_features]
+        
+        imputer = ColumnTransformer(
+            transformers=[
+                ("num", SimpleImputer(strategy="mean"), numerical_features),
+                ("cat", SimpleImputer(strategy="most_frequent"), categorical_features),
+            ],
+            remainder="passthrough",
+        )
+        
+        X_train = imputer.fit_transform(X_train_df)
+        X_test = imputer.transform(X_test_df)
+    
+    if onehot:
+        if not impute:
+            def make_pd_from_np(x):
+                data = pd.DataFrame(x)
+                for c in categorical_features:
+                    data.iloc[:, c] = data.iloc[:, c].astype("category")
+                return data
+            X_train, X_test = make_pd_from_np(X_train), make_pd_from_np(X_test)
+        
         transformer = ColumnTransformer(
             transformers=[
                 (
                     "cat",
-                    OneHotEncoder(handle_unknown="ignore", sparse=False),
+                    OneHotEncoder(handle_unknown="ignore", sparse_output=False),
                     categorical_features,
                 )
             ],
             remainder="passthrough",
         )
-
         X_train = transformer.fit_transform(X_train)
         X_test = transformer.transform(X_test)
-
+    
     if standardize:
         scaler = MinMaxScaler()
         X_train = scaler.fit_transform(X_train)
         X_test = scaler.transform(X_test)
-
+    
     return X_train, y_train, X_test, y_test
